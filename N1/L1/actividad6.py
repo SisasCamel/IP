96838d8c906cd090fce703908c83461fab39901d
SisasCamel <gsanchezv07@outlook.es>
2024-08-09 10:05:30 -0500

    Primera clase de laboratorio, suma de dos numeros.

---
+++ N1/L1/actividad6.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 09:42:42 2024
+
+@author: gsanc
+"""
+
+def suma(num1: int, num2: int) -> int:
+    '''
+    
+    Parametros
+    ----------
+    num1 : int
+        El primer numero entero.
+    num2 : int
+        El segundo numero entero.
+
+    Returns
+    -------
+    int
+        Devuelve la suma de dos enteros.
+
+    '''
+    return num1 + num2
+
+print(suma(10,-10))
+    
